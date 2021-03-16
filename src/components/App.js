a471eb16436b1ffdb512f97dd20cd42646245454
“Aaron <github email address>
2021-03-16 15:12:25 -0400

    completed with MVP

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,13 @@
-import video from "../data/video.js";
+// import video from "../data/video.js";
+import Details from "./Details.js"
 
 function App() {
-  console.log("Here's your data:", video);
+  // console.log("Here's your data:", video);
 
   return (
     <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameborder="0"
-        allowfullscreen
-        title="Thinking in React"
-      />
+      < Details />
+
     </div>
   );
 }
---
+++ src/components/Details.js
@@ -0,0 +1,52 @@
+import React, {useState} from "react";
+import video from "../data/video.js";
+
+function Details() {
+    const [upvotes, setUpvotes] = useState(video.upvotes)
+    const [downvotes, setDownvotes] = useState(video.downvotes)
+    const [hideComments, setHideComments] = useState(false)
+
+    function handleUpvoteClick() {
+        setUpvotes(upvotes + 1)
+    }
+
+    function handleDownvoteClick() {
+        setDownvotes(downvotes + 1)
+    }
+
+    function handleHideComments() {
+        setHideComments(!hideComments);
+        console.log(hideComments)
+    }
+return (
+    
+    <div className="details">
+            <iframe
+        width="919"
+        height="525"
+        src={video.embedUrl}
+        frameborder="0"
+        allowfullscreen
+        title="Thinking in React"
+      />
+
+        <p> {video.title} </p>
+        <p> {video.views} </p>
+        <button className="upvotes" onClick={handleUpvoteClick}> 👍 {upvotes} </button>
+        <button className="downvotes" onClick={handleDownvoteClick}> 👎 {downvotes} </button>
+        <p>
+        <button className="hideComments" onClick={handleHideComments}> 
+            {hideComments ? "Hide" : "Show"} Comments </button>
+        </p>
+        {hideComments ?
+        <p> {video.comments[0].user} </p>
+        : null}
+        {hideComments ?
+        <p> {video.comments[0].comment} </p>
+        : null}
+        {hideComments ?
+        <p> {video.comments[1].comment} </p>
+        :null}
+        </div>
+)}
+export default Details;
